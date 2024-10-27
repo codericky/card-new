aa92a67dc3daf45fe98189afdc828a7bdafabccd
codericky <thevenom425@gmail.com>
2024-10-27 12:59:38 +0700

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",],
   theme: {
     extend: {},
     fontFamily: {
