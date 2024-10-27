57a50b7d6523213ecee7d88de6ae03a431254337
codericky <thevenom425@gmail.com>
2024-10-27 09:09:49 +0700

    add repository

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      style1: ['Halant'],
+      Rampart: ['Rampart One', 'Cursive']
+    },
+  },
+  plugins: [],
+}
